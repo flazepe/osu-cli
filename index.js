b085bcaf9408b560b094b7ea5230be03453da817
flazepe <51487578+flazepe@users.noreply.github.com>
2020-06-09 23:29:10 +0800

    Remove node-fetch
    Use native https module in favor of node-fetch.

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const readline = require("readline");
-const fetch = require("node-fetch");
+const {get} = require("https");
+const {apiKey} = require("./config.json");
 
-const { apiKey } = require("./config.json");
-const modes = { "standard": "0", "taiko": "1", "catch": "2", "mania": "3" };
+const modes = {
+	"standard": "0", "s": "0",
+	"taiko": "1", "t": "1",
+	"catch": "2", "c": "2",
+	"mania": "3", "m": "3"
+};
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-function ask(question) {
-	return new Promise(resolve => rl.question(question, data => resolve(data)));
-}
+function ask(question) { return new Promise(resolve => rl.question(question, data => resolve(data))); }
 
 (async () => {
 	const name = await ask("Provide the player name:\n");
 	
 	const mode = await (async () => {
-		let data = await ask("Provide the mode (standard, taiko catch, or mania):\n");
+		let data = await ask("Provide the mode - standard (s), taiko (t), catch (c), or mania (m):\n");
 		data = data.toLowerCase();
 		
 		if (!modes[data]) {
@@ -29,23 +32,29 @@ function ask(question) {
 	})();
 	
 	rl.close();
-	
 	console.log("Fetching user...");
-	const res = await fetch(`https://osu.ppy.sh/api/get_user?k=${apiKey}&m=${modes[mode]}&u=${encodeURIComponent(name)}`)
 	
-	let json = await res.json();
-	if (!json[0]) return console.log("User not found.");
-	json = json[0];
-	
-	console.log("\n\n" + [
-		["Name", `${json.username} (#${parseInt(json.pp_rank).toLocaleString()})`],
-		["ID", json.user_id],
-		["Country", `${json.country} (#${parseInt(json.pp_country_rank).toLocaleString()})`],
-		["Created", json.join_date],
-		["Performance Points", `${Math.floor(json.pp_raw).toLocaleString()}pp`],
-		["Level", Math.floor(json.level)],
-		["Accuracy", `${parseFloat(json.accuracy).toFixed(2)}%`],
+	get(`https://osu.ppy.sh/api/get_user?k=${apiKey}&m=${modes[mode]}&u=${encodeURIComponent(name)}`, res => {
+		let data = "";
 		
-		["Play Count", json.playcount ? parseInt(json.playcount).toLocaleString() : 0]
-	].map(x => `${x[0]}: ${x[1]}`).join("\n"));
+		res.on("data", chunk => data += chunk);
+		
+		res.on("end", () => {
+			let json = JSON.parse(data);
+			if (!json[0]) return console.log("User not found.");
+			json = json[0];
+			
+			console.log("\n" + [
+				["Name", `${json.username} (#${parseInt(json.pp_rank).toLocaleString()})`],
+				["ID", json.user_id],
+				["Country", `${json.country} (#${parseInt(json.pp_country_rank).toLocaleString()})`],
+				["Created", json.join_date],
+				["Performance Points", `${Math.floor(json.pp_raw).toLocaleString()}pp`],
+				["Level", Math.floor(json.level)],
+				["Accuracy", `${parseFloat(json.accuracy).toFixed(2)}%`],
+				
+				["Play Count", json.playcount ? parseInt(json.playcount).toLocaleString() : 0]
+			].map(x => `${x[0]}: ${x[1]}`).join("\n"));
+		});
+	});
 })();
